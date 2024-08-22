ca17f02eaf9d305debd6a932a65f8f486a9e800b
kingofmidgard <satyaprakashsingh70675@gmail.com>
2024-08-22 17:09:30 +0530

    SpiralMatrix

--- 7.2D-Array/spiralMatrix.cpp
+++ 7.2D-Array/spiralMatrix.cpp
@@ -47,3 +47,47 @@ int main(){
     spiralmatrix(arr,n,m);
     return 0;
 }
+
+
+
+///using pointer..
+/*Note: The returned array must be malloced, assume caller calls free().
+
+int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
+    int n=matrixSize;
+    int m=*matrixColSize;
+    int *ptr=(int*)malloc((m*n)*sizeof(int));
+    *returnSize=m*n;
+    int srow=0,scol=0,erow=n-1,ecol=m-1;
+    int k=0;
+    while(srow<=erow && scol<=ecol ){
+    //top
+    for(int j=scol;j<=ecol;j++){
+        ptr[k++]=matrix[srow][j];
+    }
+    //right
+    for(int i=srow+1;i<=erow;i++){
+       ptr[k++]=matrix[i][ecol];
+    }
+    //bottom
+    for(int j=ecol-1;j>=scol;j--){//once look at inequality sign at time of revision...
+        if(srow==erow){//to be safe from duplicate printing
+        break;
+        }
+        ptr[k++]=matrix[erow][j];
+    }
+    //left
+    for(int i=erow-1;i>=srow+1;i--){
+        if(scol==ecol){//to be safe from duplicate printing
+        break;
+        }
+        ptr[k++]=matrix[i][scol];
+    }
+    srow++;
+    erow--;
+    scol++;
+    ecol--;
+    }
+    return ptr;
+}
+*/
