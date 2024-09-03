e40b9bc4fe55cc4aa537b9aaf8ba9044bff2bd6c
kingofmidgard <singhsatyaprakash70675@gmail.com>
2024-09-03 10:54:43 +0530

    Lab

---
+++ 00.LAB/11bankAccount.cc
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+// #include "mylib.cc"
+using namespace std;
+//Bank Account..
+class Bank{
+    private:
+        string Depositer;
+        string AccountNumber;
+        string TypeAcccount;
+        double balance;
+    public:
+        Bank(string Depositer,string AccountNumber,string TypeAcccount,double balance){
+            this->Depositer=Depositer;
+            this->AccountNumber=AccountNumber;
+            this->TypeAcccount=TypeAcccount;
+            this->balance=balance;
+        }
+        void deposit(double amount){
+            balance+=amount;
+        }
+        double Dispalybalance(){
+            return balance;
+        }
+        void withdrawal(double withdr){
+            if(balance<withdr){
+                cout<<"Insufficient bank balance..."<<endl;
+                return;
+            }
+            else{
+            balance-=withdr;
+            cout<<"Your money debited sucessfully.."<<endl;
+            }
+        } 
+        void displayDetails(){
+            cout<<"Account Holder"<<setw(26)<<"Acoount Number"<<setw(26)<<"Account Type"<<setw(26)<<"Balance"<<endl;
+            cout<<Depositer<<setw(26)<<AccountNumber<<setw(26)<<TypeAcccount<<setw(26)<<balance<<endl;
+        }
+};
+int main(){
+    string vname,vacc,vtype;
+    double vbalance;
+    cout<<"Enter Account Holder name:";
+    getline(cin,vname);
+    cout<<"Enter Account Number:";
+    getline(cin,vacc);
+    cout<<"Enter Account Type:";
+    getline(cin,vtype);
+    cout<<"Enter Bank Balance:";
+    cin>>vbalance;
+    Bank c1(vname,vacc,vtype,vbalance);
+    int n;
+    do{
+    cout<<"*******Main Menu********"<<endl;
+    cout<<"Press[1] for Deposit..."<<endl;
+    cout<<"Press[2] for CheckBalance..."<<endl;
+    cout<<"Press[3] for Withdrawn..."<<endl;
+    cout<<"Press[4] for Details..."<<endl;
+    cout<<"Press:";
+    cin>>n;
+    switch(n){
+        case 1:
+        {
+        cout<<"How much money have to deposit:";
+                double D;
+                cin>>D;
+                cout<<"Your current balance is:"<<c1.Dispalybalance()<<endl; 
+                c1.deposit(D);
+                cout<<"Your Amount has been deposited.."<<endl;
+                cout<<" Now,Your current balance is:"<<c1.Dispalybalance()<<endl; 
+                break;
+        }
+        case 2:
+        {
+                cout<<"Your current balance is:"<<c1.Dispalybalance()<<endl; 
+                break;
+        }
+        case 3:
+        {
+                cout<<"How much you want to withdraw:";
+                double w;
+                cin>>w;
+                c1.withdrawal(w);
+                cout<<"Your current balance is:"<<c1.Dispalybalance()<<endl;    
+                break;
+        }
+        case 4:
+        {
+                c1.displayDetails();
+                break;
+        }
+        case 0:
+        {
+                cout<<"Thank You For Visit..."<<endl;
+                break;
+        }
+        default:
+        cout<<"Invalid Option"<<endl;
+    }
+    } while(n!=0);
+    return 0;
+}
---
+++ 00.LAB/14.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Score{
+    int score;
+    public:
+        Score():score(0){}
+        Score(int val):score(val){}
+        Score& operator ++ (){
+            cout<<"Pre-fix called."<<endl;
+            score+=10;
+            return *this;
+        }
+        Score operator ++ (int){//Postfix
+        cout<<"Post-fix called."<<endl;
+            Score temp=*this;
+            score+=10;
+            return temp;//return original 
+        }
+        void display() const{
+            cout<<"Score:"<<score<<endl;
+        }
+};
+int main(){
+    Score s1(60);
+    ++s1;
+    s1.display();
+    s1++;
+    s1.display();
+    return 0;
+}
