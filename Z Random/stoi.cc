4be6bb3e5601c19b3112156bf3be4cd063656f7c
kingofmidgard <singhsatyaprakash70675@gmail.com>
2025-04-25 17:38:13 +0530

    DSA with CPP

---
+++ Z Random/stoi.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string str = "02";
+    int num = std::stoi(str);
+    std::cout << "Integer: " << num << std::endl;
+    return 0;
+}
