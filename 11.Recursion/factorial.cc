53368467b7c207709bf67da735160c87717fcfe9
kingofmidgard <singhsatyaprakash70675@gmail.com>
2025-06-29 21:55:33 +0530

    Factorial

--- 11.Recursion/factorial.cc
+++ 11.Recursion/factorial.cc
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
-int factorial(int n){
-    if(n==1){
+long long factorial(long long n){
+    if(n==1|| n==0){
         return 1;
     }
     return n*factorial(n-1);
 }
 int main(){
-    int n;
+    long long n;
     cout<<"Enter num:";
     cin>>n;
     cout<<factorial(n)<<endl;
