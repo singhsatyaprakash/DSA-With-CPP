9add4e140fedab291d892a77b0010563b5ced31c
singhsatyaprakash <singhsatyaprakash70675@gmail.com>
2026-04-09 21:34:16 +0530

    Middle of Linked List

---
+++ Morgan Stanley/OASheet1/06MiddleOfLinkedList.cc
@@ -0,0 +1,24 @@
+/*
+class Node {
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}; */
+
+class Solution {
+  public:
+    int getMiddle(Node* head) {
+        Node* slow=head;
+        Node* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow->data;
+    }
+};
